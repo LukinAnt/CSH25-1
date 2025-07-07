fea23b851db64c62df626fb43c25974485fa89fa
Lukin Lukina <a.lukin@rambler.ru>
2025-07-07 22:59:49 +0300

    save Task02

---
+++ Task02/Program.cs
@@ -0,0 +1,19 @@
+﻿/*02 напишите программу, которая на вход 
+принимает два числа и выдаеткакое число больше
+5 и 7   7
+2 и 10 10
+-9 и -3   -3*/
+
+Console.WriteLine("Введите 2 числа: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number1 > number2)
+{
+    Console.Write("большее число: ");
+    Console.WriteLine(number1);
+}
+else
+{
+    Console.Write("большее число: ");
+    Console.WriteLine(number2);
+}
