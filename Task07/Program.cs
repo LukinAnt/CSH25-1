166830a695705021de1dc9d2f518b46972535dbb
Lukin Lukina <a.lukin@rambler.ru>
2025-07-07 20:23:56 +0300

    save Task07

---
+++ Task07/Program.cs
@@ -0,0 +1,10 @@
+﻿//введите трехзначное число и покажите последнюю цифру
+
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+// защита от дурака на трехзначность
+if (number > 99 && number < 1000)
+{Console.WriteLine(number % 10);}
+else
+{Console.WriteLine("не трехзначное");}
