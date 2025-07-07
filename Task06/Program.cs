698974536a61ed6bac930cebc84edc8b57c5ff54
Lukin Lukina <a.lukin@rambler.ru>
2025-07-07 23:12:39 +0300

    save Task06

---
+++ Task06/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+напишите программу, которая на вход принимает число 
+и выдает является ли число четным(делится ли оно на 2 без остатка
+)
+*/
+Console.WriteLine("Введите 1 число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+int number2 = number1 % 2;
+if (number2 == 0)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
