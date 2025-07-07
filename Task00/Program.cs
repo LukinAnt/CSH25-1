693d8a6cad5ac745b2215577241d00c90c14eb72
Lukin Lukina <a.lukin@rambler.ru>
2025-07-07 18:26:32 +0300

    save Task00

---
+++ Task00/Program.cs
@@ -0,0 +1,12 @@
+﻿/*
+ Напишите программу, которая на вход принимает 
+число и выдает его квадрат
+4 - 16
+-3  9
+-7  49
+*/
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+//для ввода числа с консоли
+Console.WriteLine(number * number);
